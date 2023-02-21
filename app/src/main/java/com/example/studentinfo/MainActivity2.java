623b6ffbc858ded494e07412e41738a11103d7f0
Jovie <119099153+codeHobi@users.noreply.github.com>
2023-02-21 15:53:08 +0800

    added some functions

--- app/src/main/java/com/example/studentinfo/MainActivity2.java
+++ app/src/main/java/com/example/studentinfo/MainActivity2.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
     TextView showStudnum, showStudname, showStudsection;
-    EditText classPart, quiz, perfTask, exam;
+    EditText classPart;
+    EditText quiz;
+    EditText perfTask;
+    EditText exam;
+    TextView equiv;
+    Button viewDialog, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,10 @@ public class MainActivity2 extends AppCompatActivity {
         quiz = (EditText) findViewById(R.id.etQuiz);
         perfTask = (EditText) findViewById(R.id.etTaskperf);
         exam =(EditText) findViewById(R.id.etExam);
+        equiv = (TextView) findViewById(R.id.tvEquivalent);
+
+        viewDialog = (Button) findViewById(R.id.btnView);
+        cancel = (Button) findViewById(R.id.btnCancel);
 
         Intent act2 = getIntent();
         String sNum = act2.getStringExtra("STUDENTNUM");
@@ -36,5 +48,44 @@ public class MainActivity2 extends AppCompatActivity {
         showStudnum.setText(sNum);
         showStudname.setText(lName + ", " + fName + " " + mInitial + ". ");
         showStudsection.setText(sec);
+
+        classPart.addTextChangedListener(computeTW);
+        quiz.addTextChangedListener(computeTW);
+        perfTask.addTextChangedListener(computeTW);
+        exam.addTextChangedListener(computeTW);
+        equiv.addTextChangedListener(computeTW);
     }
+
+    private TextWatcher computeTW = new TextWatcher() {
+        //Double classpnum = Double.parseDouble(classpartInput);
+        //Double quiznum = Double.parseDouble(quizInput);
+        //Double tasknum = Double.parseDouble(perftaskInput);
+        //Double examnum = Double.parseDouble(examInput);
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String classpartInput = classPart.getText().toString().trim();
+            String quizInput = quiz.getText().toString().trim();
+            String perftaskInput = perfTask.getText().toString().trim();
+            String examInput = exam.getText().toString().trim();
+
+            viewDialog.setEnabled(!classpartInput.isEmpty() && !quizInput.isEmpty()
+                    && !perftaskInput.isEmpty() && !examInput.isEmpty());
+            cancel.setEnabled(!classpartInput.isEmpty() || !quizInput.isEmpty()
+                    || !perftaskInput.isEmpty() || !examInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //Double equinum = ((classpnum * 0.1) + (quiznum * 0.2) + (tasknum * 0.3) + (examnum * 0.4)) / 4;
+            //String finalequiv = equinum.toString();
+
+            //equiv.setText(String.format("%.2f", equinum));
+        }
+    };
 }
